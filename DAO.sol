ab6af5791a237d2ae418beac82ee37329b2e0ff2
Juan Fuente <jfuentet@gmail.com>
2023-09-07 12:33:17 +0200

    07-09-23

--- DAO.sol
+++ DAO.sol
@@ -7,7 +7,7 @@ interface IPayload {
  
 contract Governance {
  
- //Un struct Propuesta contiene todos los datos de la propuesta
+ //Un struct Propuesta contiene todos los datos de la propuesta, le da estructura
     struct Propuesta {
         address creador;
         string descripcion;
@@ -21,22 +21,41 @@ contract Governance {
         bool ejecutado;
     }
  
-    // Del id de la propuesta a los datos de la propuesta
+    // Del id de la propuesta a los datos de la propuesta, para identificar a cada una por numero
     mapping(uint256 => Propuesta) propuestas;
-    //variable que identifica a cada propuesta
+    //variable que identifica a cada propuesta con un numero
     uint256 numPropuestas;
  
     //funcion para crear propuestas
     function crearPropuesta(
+        //se declaran las diguientes variables 
         string calldata descripcion,
         address payload
     ) public {
+        //se crea una variable propuesta de tipo propuesta como un numero identificador y se guarda en storage
         Propuesta storage propuesta = propuestas[numPropuestas];
- 
-        
+        //se da valor a los siguientes elementos del struct Propuesta para esta propuesta concreta, que la identiica el numero numPropuestas
         propuesta.creador = msg.sender;
         propuesta.descripcion = descripcion;
         propuesta.payload = payload;
- 
+        //se aumenta un numero en el numero identificador para asociarlo a la siguiente propuesta
         ++numPropuestas;
     }
+
+        function votarPropuesta(uint256 id, uint256 voto) public {
+        Propuesta storage propuesta = propuestas[id];
+ 
+        require(!propuesta.votado[msg.sender], "Ya has votado!");
+        require(voto < 3, "Voto incorrecto");
+ 
+        if (voto == 0) {
+            propuesta.numeroSi++;
+        } else if (voto == 1) {
+            propuesta.numeroNo++;
+        } else if (voto == 2) {
+            propuesta.numeroBlancos++;
+        }
+ 
+        propuesta.votado[msg.sender] = true;
+    }
+ 
