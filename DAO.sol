5a6cb34d76c821e66bb599228a29f22425e3f562
Juan Fuente <jfuentet@gmail.com>
2023-09-07 13:51:29 +0200

    07-09-23

--- DAO.sol
+++ DAO.sol
@@ -26,28 +26,29 @@ contract Governance {
     //variable que identifica a cada propuesta con un numero
     uint256 numPropuestas;
  
-    //funcion para crear propuestas
+    //funcion para crear propuestas que toma por parametro la descripcion y la direccion de payload
     function crearPropuesta(
-        //se declaran las diguientes variables 
         string calldata descripcion,
         address payload
     ) public {
         //se crea una variable propuesta de tipo propuesta como un numero identificador y se guarda en storage
         Propuesta storage propuesta = propuestas[numPropuestas];
-        //se da valor a los siguientes elementos del struct Propuesta para esta propuesta concreta, que la identiica el numero numPropuestas
+        //se da valor a los siguientes elementos del struct Propuesta para esta propuesta concreta, que la identiica con el numero numPropuestas
         propuesta.creador = msg.sender;
         propuesta.descripcion = descripcion;
         propuesta.payload = payload;
         //se aumenta un numero en el numero identificador para asociarlo a la siguiente propuesta
         ++numPropuestas;
     }
-
+        //funcion para votar propuestas, toma por parametro el numero para identificar propuesta y el voto
         function votarPropuesta(uint256 id, uint256 voto) public {
         Propuesta storage propuesta = propuestas[id];
- 
+        //se evalua que ¿¿¿¿¿¿¿se haya votado ya, por defecto esta en false????????????
         require(!propuesta.votado[msg.sender], "Ya has votado!");
+        //se evalua que el voto no sea mayor que 3, si lo es seria erroneo
         require(voto < 3, "Voto incorrecto");
  
+        //se asocia capa voto con un valor de 0 a 2 y se va incrementando su cantidad dentro de propuesta
         if (voto == 0) {
             propuesta.numeroSi++;
         } else if (voto == 1) {
@@ -56,6 +57,7 @@ contract Governance {
             propuesta.numeroBlancos++;
         }
  
+        //se indica que esa direccion ya ha votado en el valor indicado dentro de prpuesta
         propuesta.votado[msg.sender] = true;
     }
  
